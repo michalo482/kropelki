ca6ecbcf1354dde135aea67d14128f5949e8758e
dakko <dakkon@o2.pl>
2021-08-01 14:46:51 +0200

    pierwszy wypych

---
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.mygdx.game.Drop;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Drop");
+		config.setWindowedMode(800, 480);
+		new Lwjgl3Application(new Drop(), config);
+	}
+}
